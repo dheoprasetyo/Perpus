21f5a31d26a51b3483fe80a0b30b4c8a9b0db507
dheoprasetyo <dheoprasetyo.dp@gmail.com>
2020-07-07 00:55:55 +0700

    change login display

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="{{ asset('adminlte/index2.html')}}"><b>Admin</b>LTE</a>
+    <a href="{{ asset('adminlte/index2.html')}}"><b>Perpustakaan</b></a>
     @include('layouts._flash')
   </div>
   <!-- /.login-logo -->
